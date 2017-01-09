982f1bdad7a670ecd6d0c11f62350db2676851d3
kaoqibutaitou <kaoqibutaitou@foxmail.com>
2017-01-09 17:47:26 +0800

    初步设计IApp接口

--- src/com/kaoqibutaitou/bit/tools/impl/IAppImpl.java
+++ src/com/kaoqibutaitou/bit/tools/impl/IAppImpl.java
@@ -6,28 +6,25 @@ import com.kaoqibutaitou.bit.tools.inter.IApp;
  * Created by Yun on 2017/1/9.
  */
 public abstract class IAppImpl<Result> implements IApp<Result>{
+    private AppState state;
+    public IAppImpl(String [] args) {
+        super();
+        state = AppState.NoError;
+        if(!initParams(args)) {
+            state = AppState.InitParamError;
+        }
+    }
+
     @Override
-    public boolean initParams(String[] args) {
-        return false;
+    public AppState getState() {
+        return state;
     }
 
     @Override
     public void help() {
-
-    }
-
-    @Override
-    public void run() {
-
-    }
-
-    @Override
-    public Result getResult() {
-        return null;
-    }
-
-    @Override
-    public void display() {
-
+        if(state!=AppState.NoError){
+            System.out.println("Error:" + state.getStateInfo());
+        }
+        System.out.println(getExecuteCmdString());
     }
 }
--- src/com/kaoqibutaitou/bit/tools/inter/IApp.java
+++ src/com/kaoqibutaitou/bit/tools/inter/IApp.java
@@ -6,7 +6,36 @@ package com.kaoqibutaitou.bit.tools.inter;
 public interface IApp<Result> {
     boolean initParams(String [] args);
     void help();
-    void run();
+    AppState run();
+    AppState getState();
     Result getResult();
     void display();
+    String getExecuteCmdString();
+
+    enum AppState{
+        InitParamError(-1,"Init Param failed!"),
+        NoError(0,"No Error"),
+        UnRun(1,"Don't invoke the run method!"),
+        RuntimeError(2);
+
+        AppState(int stateCode) {
+            this.stateCode = stateCode;
+        }
+
+        AppState(int stateCode, String stateInfo) {
+            this.stateCode = stateCode;
+            this.stateInfo = stateInfo;
+        }
+
+        private int stateCode;
+        private String stateInfo;
+
+        public String getStateInfo() {
+            return stateInfo;
+        }
+
+        public void setStateInfo(String stateInfo) {
+            this.stateInfo = stateInfo;
+        }
+    }
 }
